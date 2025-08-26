1f3a0989dab7d848009571dc042e28b47afa9c1b
theakasharma <akasharma303@gmail.com>
2025-08-26 19:27:54 +0530

    Update

--- src/app/Challenge/day2/otpValidate.jsx
+++ src/app/Challenge/day2/otpValidate.jsx
@@ -73,50 +73,58 @@ export default function OtpWithLeva() {
 
 
   return (
-    <div className="flex h-dvh flex-col justify-center">
-      <h1 className=" text-5xl text-center" style={{ fontFamily: "var(--font-instrument-serif)" }}>OTP Verification Challenge</h1>
-      <p className="text-md mt-1.5 text-center">Please enter the correct OTP given below</p>
-      <div className="flex flex-col gap-6 items-center mt-10">
-        <div className={`flex gap-3 ${status === "incorrect" && "animate-shake"}`}>
-          {otp.map((digit, i) => (
-            <input
-              key={i}
-              type="text"
-              maxLength={1}
-              ref={(el) => (inputsRef.current[i] = el)}
-              onKeyDown={(e) => handleKeyDown(e, i)}
-              value={digit}
-              onChange={(e) => handleChange(e.target.value, i)}
-              className={boxStyle(i) + " text-center"}
-            />
-          ))}
-        </div>
-        {status === "correct" ? (
-          <p
-            className={`text-sm ${status === "correct"
-              ? "text-green-600 animate-otp-bounce"
-              : status === "incorrect"
-                ? "text-red-600 animate-shake "
-                : "text-gray-500"
-              }`}
-          >
-            {status === "correct"
-              ? "OTP Verified"
-              : status === "incorrect"
-              && "❌ Wrong OTP"
-            }
-          </p>
-        ) : (
-          <p className="text-sm">
-            Verified Code: <span className=" rounded-4xl border-gray-400 border bg-gray-100 px-2.5 py-1">
-              {validOtp}
-            </span>
-          </p>
-        )}
-        {/* Leva control panel */}
-        <Leva collapsed={false} />
+    <>
+      <div className="header">
+        <a className="bear-link" href="https://peerlist.io/akashamra" target="_blank" rel="noreferrer noopener">
+          <img src="https://avatars.githubusercontent.com/u/62895760?v=4" alt="Profile" className=" w-12 h-12"/>
+            <h4>AAKASH SHARMA</h4>
+        </a>
       </div>
+      <div className="flex h-dvh flex-col justify-center">
+        <h1 className=" text-5xl text-center" style={{ fontFamily: "var(--font-instrument-serif)" }}>OTP Verification Challenge</h1>
+        <p className="text-md mt-1.5 text-center">Please enter the correct OTP given below</p>
+        <div className="flex flex-col gap-6 items-center mt-10">
+          <div className={`flex gap-3 ${status === "incorrect" && "animate-shake"}`}>
+            {otp.map((digit, i) => (
+              <input
+                key={i}
+                type="text"
+                maxLength={1}
+                ref={(el) => (inputsRef.current[i] = el)}
+                onKeyDown={(e) => handleKeyDown(e, i)}
+                value={digit}
+                onChange={(e) => handleChange(e.target.value, i)}
+                className={boxStyle(i) + " text-center"}
+              />
+            ))}
+          </div>
+          {status === "correct" ? (
+            <p
+              className={`text-sm ${status === "correct"
+                ? "text-green-600 animate-otp-bounce"
+                : status === "incorrect"
+                  ? "text-red-600 animate-shake "
+                  : "text-gray-500"
+                }`}
+            >
+              {status === "correct"
+                ? "OTP Verified"
+                : status === "incorrect"
+                && "❌ Wrong OTP"
+              }
+            </p>
+          ) : (
+            <p className="text-sm">
+              Verified Code: <span className=" rounded-4xl border-gray-400 border bg-gray-100 px-2.5 py-1">
+                {validOtp}
+              </span>
+            </p>
+          )}
+          {/* Leva control panel */}
+          <Leva collapsed={false} />
+        </div>
 
-    </div>
+      </div>
+    </>
   );
 }
